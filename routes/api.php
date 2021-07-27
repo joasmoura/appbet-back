108ce98c7386b0366b7f44005135c73a3dfe97b7
joasmoura <40508788+joasmoura@users.noreply.github.com>
2021-07-27 17:35:19 -0300

    Atualizar remoção classe userController

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login',[UserController::class,'login']);
+Route::post('/login',[UsuarioController::class,'login']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
